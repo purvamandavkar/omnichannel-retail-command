277a90060c0538b6e2f7fbdba2e5ddff2e9db99c
purvamandavkar <mandavkarpurva99@gmail.com>
2026-05-16 12:47:38 +0530

    Add .gitignore to exclude node_modules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,6 +5,12 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
-        proxy: { '/api': 'http://localhost:5000' }
+        proxy: {
+            '/api': 'http://localhost:5000'
+        }
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: false
     }
 })
